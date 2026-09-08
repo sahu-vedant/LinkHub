fb3d22e02f72564bb6b71b8ad0726403fc58ca3f
Vedant Sahu <88942026+sahu-vedant@users.noreply.github.com>
2026-09-08 22:39:52 +0530

    Update websites.js

--- websites.js
+++ websites.js
@@ -40,6 +40,7 @@ const websites = [
     url: "https://matplotlib.org/stable/index.html",
     category: "Python",
     description: "Matplotlib Documentation",
+    logo: "https://miro.medium.com/v2/1*I0t-fLjyUZ3XnJLRW1B-uQ.png".
   },
   {
     name: "Seaborn",
